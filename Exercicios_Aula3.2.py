46d83648e88403c008763a7cf59468d8c25d3276
SLoWLY-13 <ogoid2000@gmail.com>
2025-04-04 19:45:03 +0100

    Add exercises 2 to 8 and match case example; remove empty file

--- Exercicios_Aula3.2.py
+++ Exercicios_Aula3.2.py
@@ -1,1 +1,23 @@
 # Exercício 2: Fazer um programa que analise 3 valores inteiros (através das variáveis num1, num2 e num3), e informa qual o maior e qual o menor deles.  
+
+num1=0
+num2=0
+num3=0
+
+num1=int(input("Digite um primeiro numero: "))
+num2=int(input("Digite um segundo numero:"))
+num3=int(input("Digite um terceiro numero:"))
+
+
+if num1 > num2 and num2 > num3:
+    print("num 1 é o maior, num 3 é o menor")
+elif num1 > num3 and num3 > num2:
+    print("num 1 é o maior, num 2 é o menor")
+elif num2 > num1 and num1 > num3:
+    print("num 2 é o maior , num 3 é o menor")
+elif num2 > num3 and num3 > num1:
+    print("num 3 é o maior, num 1 é o menor ")
+elif num3 > num2 and num2 > num1:
+    print("num 3 é o maior, num 1 é o menor")
+elif num3 > num1 and num1 > num2:
+    print("num 3 é o maior, num 2 é o menor")
